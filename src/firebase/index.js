b1add26f322321ce8a3a28319a8557fa85ff8188
Javier <jamarks@gmail.com>
2020-09-09 01:53:48 +0200

    firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,22 @@
+import * as firebase from "firebase/app"
+import "firebase/firestore"
+
+const app = firebase.initializeApp({
+    apiKey: "AIzaSyBDgeH8RsYg874jHxWaJX-eePvOUK-OMtQ",
+    authDomain: "ecommerce-wasserman.firebaseapp.com",
+    databaseURL: "https://ecommerce-wasserman.firebaseio.com",
+    projectId: "ecommerce-wasserman",
+    storageBucket: "ecommerce-wasserman.appspot.com",
+    messagingSenderId: "570005213077",
+    appId: "1:570005213077:web:b87c22367fddb58978f09c",
+    measurementId: "G-GHL80WKV4G"
+  });
+
+
+  export function getFirease(){
+      return app;
+  }
+
+  export function getFirestore(){
+      return firebase.firestore(app);
+  }
