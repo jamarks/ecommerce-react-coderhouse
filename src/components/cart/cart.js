5de91e233aa907cec53507429b4857047ce87231
Javier Wasserman <jamarks@gmail.com>
2020-09-15 07:18:10 +0200

    mejora en el cart

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -17,23 +17,28 @@ export default ()=>{
     const [phone, setPhone] = useState('');
     const [email, setEmail] = useState('');
     const [confirmEmail, setConfirmEmail] = useState('');
+    const [enableSubmit, setEnableSubmit] = useState(false)
+    
 
-    //console.log(cart);
+    
 
-    const submitValue = () => {
-        const frmdetails = {
-            'First Name' : fName,
-            'Last Name' : lName,
-            'Phone' : phone,
-            'Email' : email
+    const validEmail = (value) =>{
+        setConfirmEmail(value);
+        console.log('email:' + email + ' confirm:' + confirmEmail);
+        if(email && confirmEmail){
+            if(email===value)
+                setEnableSubmit(true)
+            else
+                setEnableSubmit(false)
+        }else{
+            setEnableSubmit(false)
         }
-        console.log(frmdetails);
     }
 
     async function createOrder(){
         //const order = { buyer: { name , phone, email }, items: [{ id, title, price, quantity }, {}, {} ], total }
         const buyer = {
-            name:'javier', phone:'54911368657035',email:'jamarks@gmail.com'
+            name:fName, lastName:lName, phone:phone,email:email
         }
       
         const items = cart.map(c=>({
@@ -42,7 +47,6 @@ export default ()=>{
             
       
         const db = getFirestore()
-
 
         // iterar por cada item y cambiar el stock
 
@@ -96,10 +100,10 @@ export default ()=>{
                             <div className='row marginTopSmall' key={i.product.id}>
                                 <div className='col col-md-3'><img src={i.product.imageId} className='img-fluid border-right border-bottom'></img></div>
                                 <div className='col col-md-7'>
-                                    <h3>Item <small class="text-muted">{i.product.title}</small></h3>
-                                    <h6>Cantidad: <small class="text-muted">{i.count}</small></h6>
-                                    <h6>Precio: <small class="text-muted">${i.product.price}</small></h6>
-                                    <h6>Total: <small class="text-muted">${Number((i.product.price*i.count).toFixed(2))}</small></h6>
+                                    <h3>Item <small className="text-muted">{i.product.title}</small></h3>
+                                    <h6>Cantidad: <small className="text-muted">{i.count}</small></h6>
+                                    <h6>Precio: <small className="text-muted">${i.product.price}</small></h6>
+                                    <h6>Total: <small className="text-muted">${Number((i.product.price*i.count).toFixed(2))}</small></h6>
                                     <button type="button" className="btn btn-link noMarginNoPadding" onClick={()=>{removeItem(i.product.id)}}>Borrar item</button>
                                     
                                 </div>
@@ -108,34 +112,33 @@ export default ()=>{
                     </div>
                     <div className='col-md-4 offset-md-1'>
 
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item active">Finalizá tu compra</li>
-                            <li class="list-group-item no-padding">
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item active">Finalizá tu compra</li>
+                            <li className="list-group-item no-padding">
                                 <p>Datos del comprador</p>
                                 <form>
-                                    <input class="form-control" type="text" placeholder="Nombre" onChange={e => setfName(e.target.value)}/>
-                                    <input class="form-control" type="text" placeholder="Apellido" onChange={e => setlName(e.target.value)}/>
-                                    <input class="form-control" type="text" placeholder="Teléfono" onChange={e => setPhone(e.target.value)}/>
-                                    <input class="form-control" type="text" placeholder="E-mail" onChange={e => setEmail(e.target.value)}/>
-                                    <input class="form-control" type="text" placeholder="Repetir E-mails" onChange={e => setConfirmEmail(e.target.value)}/>
+                                    <input className="form-control" type="text" placeholder="Nombre" value={fName} onChange={e => setfName(e.target.value)}/>
+                                    <input className="form-control" type="text" placeholder="Apellido" value={lName} onChange={e => setlName(e.target.value)}/>
+                                    <input className="form-control" type="text" placeholder="Teléfono" value={phone} onChange={e => setPhone(e.target.value)}/>
+                                    <input className="form-control" type="text" placeholder="E-mail" value={email} onChange={e => setEmail(e.target.value)}/>
+                                    <input className="form-control" type="text" placeholder="Repetir E-mails" value={confirmEmail} onChange={e => validEmail(e.target.value)}/>
                                 </form>
                             </li>
-                            <li class="list-group-item">
-                            <h4>Items:  <small class="text-muted">{cartLenght()}</small></h4>
-                            <h4>Total:  <small class="text-muted">${getTotal()}</small></h4>
+                            <li className="list-group-item">
+                            <h4>Items:  <small className="text-muted">{cartLenght()}</small></h4>
+                            <h4>Total:  <small className="text-muted">${getTotal()}</small></h4>
                             </li>
-                            <li class="list-group-item">
-                            <button onClick={()=> submitValue()} type="button" className="btn btn-primary disabled"> Comprar </button>
-                            </li>
-                        </ul>
-                        
-                        {userOrderId  && <>
-                            <div className='row'>
-                                Order Creada: # {userOrderId}
-                            </div>  
+                            <li className="list-group-item">
+                                {enableSubmit && <button onClick={()=> createOrder()} type="button" className="btn btn-primary"> Comprar </button>}
+                                {!enableSubmit && <button onClick={()=> alert('Los emails no coinciden')} type="button" className="btn btn-primary disabled"> Comprar </button>}
+                                {userOrderId  && <> <div className='row'> Order Creada: # {userOrderId}</div>  
                             </>   
                 
                         }
+                            </li>
+                        </ul>
+                        
+                        
                     </div>
                 </div>
             </>
